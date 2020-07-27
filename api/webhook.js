e713e692c880e3430b6fd0e4896952f2e5a53a44
Igor Makarsky <makarrski@gmail.com>
2020-07-27 21:43:12 +0300

    Fix webhook method

--- api/webhook.js
+++ api/webhook.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req,res) => {
+router.post('/', (req,res) => {
 	res.json({
 		"fulfillmentMessages": [
 			{
