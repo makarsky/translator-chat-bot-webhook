16df405466ac7f8903009e3582c429cc7c48aa45
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2020-07-27 22:43:00 +0300

    Log webhook request body

--- api/webhook.js
+++ api/webhook.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-	console.log(req);
-	
+	console.log(req.body);
+
 	res.json({
 		"fulfillmentMessages": [
 			{
