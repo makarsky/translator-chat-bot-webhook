9f6b516001688cd14f984c90b3f5e98208ebb1b4
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2020-07-28 23:06:43 +0300

    Integrate Google translation

--- api/webhook.js
+++ api/webhook.js
@@ -1,21 +1,31 @@
 const express = require('express');
+const translate = require('@vitalets/google-translate-api');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 	console.log(req.body);
+	console.log(req.body.queryResult.queryText);
+	let result = {};
+
+	try {
+		result = await translate(req.body.queryResult.queryText, {to: 'en'});
+	    console.log('result');
+	    console.log(result);
+	} catch(err) {
+	    console.error(err);
+	}
 
 	res.json({
 		"fulfillmentMessages": [
 			{
 				"text": {
 					"text": [
-						"Text response from webhook"
+						result.text
 					]
 				}
 			}
 		],
-		body: req.body
 	});
 });
 
