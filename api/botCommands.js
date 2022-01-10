0a16373622eb184d2d912748068fd2ce79e376bd
Igor Makarsky <makarrski@gmail.com>
2022-01-11 00:29:10 +0300

    Add hidden /start command. Use message.from.language_code as a default interface language

--- api/botCommands.js
+++ api/botCommands.js
@@ -3,32 +3,45 @@ const inlineButtonsBuilder = require('./inlineButtonsBuilder');
 const i18n = require('../localization/i18n');
 const availableLanguages = require('./availableLanguages');
 
+const setTargetLanguageHandler = async function (message, match) {
+    const chatSettings = await redisClient.hGetAll(`${message.chat.id}`);
+    const lastUsedLanguageCodes = JSON.parse(chatSettings.lastUsedLanguageCodes || '[]');
+    const languageCodes = lastUsedLanguageCodes.length > 0
+        ? lastUsedLanguageCodes.slice(0, inlineButtonsBuilder.maxNumberOfInlineButtons - 1)
+        : availableLanguages.map((l) => l.code).slice(0, inlineButtonsBuilder.maxNumberOfInlineButtons - 2);
+
+    this.sendMessage(
+        message.chat.id,
+        i18n.t(
+            'chooseTargetLanguage',
+            chatSettings.interfaceLanguageCode || message.from.language_code
+        ),
+        inlineButtonsBuilder.buildLanguageCodeReplyOptions(
+            languageCodes,
+            'targetLanguageCode',
+            undefined,
+            lastUsedLanguageCodes.length > 0 ? 0 : 1
+        )
+    );
+};
+
 const botCommands = [
+    {
+        regExp: /\/start/,
+        description: 'Set target language',
+        hidden: true,
+        handler: setTargetLanguageHandler,
+    },
     {
         regExp: /\/set_target_language/,
         description: 'Set target language',
-        handler: async function (message, match) {
-            const chatSettings = await redisClient.hGetAll(`${message.chat.id}`);
-            const lastUsedLanguageCodes = JSON.parse(chatSettings.lastUsedLanguageCodes || '[]');
-            const languageCodes = lastUsedLanguageCodes.length > 0
-                ? lastUsedLanguageCodes.slice(0, inlineButtonsBuilder.maxNumberOfInlineButtons - 1)
-                : availableLanguages.map((l) => l.code).slice(0, inlineButtonsBuilder.maxNumberOfInlineButtons - 2);
-
-            this.sendMessage(
-                message.chat.id,
-                i18n.t('chooseTargetLanguage', chatSettings.interfaceLanguageCode),
-                inlineButtonsBuilder.buildLanguageCodeReplyOptions(
-                    languageCodes,
-                    'targetLanguageCode',
-                    undefined,
-                    lastUsedLanguageCodes.length > 0 ? 0 : 1
-                )
-            );
-        },
+        hidden: false,
+        handler: setTargetLanguageHandler,
     },
     {
         regExp: /\/about/,
         description: 'About',
+        hidden: false,
         handler: async function (message, match) {
             const chatSettings = await redisClient.hGetAll(`${message.chat.id}`);
             // https://core.telegram.org/bots/api#formatting-options
@@ -36,7 +49,7 @@ const botCommands = [
                 message.chat.id,
                 i18n.t(
                     'about',
-                    chatSettings.interfaceLanguageCode,
+                    chatSettings.interfaceLanguageCode || message.from.language_code,
                     ['https://github.com/makarsky/translator-chat-bot-webhook']
                 ),
                 { parse_mode: 'MarkdownV2' }
@@ -46,11 +59,15 @@ const botCommands = [
     {
         regExp: /\/set_interface_language/,
         description: 'Set interface language',
+        hidden: false,
         handler: async function (message, match) {
             const chatSettings = await redisClient.hGetAll(`${message.chat.id}`);
             this.sendMessage(
                 message.chat.id,
-                i18n.t('chooseInterfaceLanguage', chatSettings.interfaceLanguageCode),
+                i18n.t(
+                    'chooseInterfaceLanguage',
+                    chatSettings.interfaceLanguageCode || message.from.language_code
+                ),
                 inlineButtonsBuilder.buildLanguageCodeReplyOptions(
                     Object.keys(i18n.chooseTargetLanguage),
                     'interfaceLanguageCode',
