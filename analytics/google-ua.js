af2234f71538d501ab10b8d6511902ba506e27b9
Igor Makarsky <makarrski@gmail.com>
2022-01-13 19:34:32 +0300

    Add universal-analytics package. Set up basic analytics events. Add .env.example

---
+++ analytics/google-ua.js
@@ -0,0 +1,31 @@
+const ua = require('universal-analytics');
+
+const categories = {
+    translator: 'translator',
+};
+
+const actions = {
+    start: 'start',
+    setTargetLanguage: 'setTargetLanguage',
+    targetLanguageSelected: 'targetLanguageSelected',
+    setInterfaceLanguage: 'setInterfaceLanguage',
+    interfaceLanguageSelected: 'interfaceLanguageSelected',
+    translate: 'translate',
+    listen: 'listen',
+    about: 'about',
+};
+
+const event = (userId, category, action, label, value, params) => {
+    const visitor = ua(
+        process.env.GOOGLE_UNIVERSAL_ANALYTICS_ID,
+        `${userId}`,
+        { strictCidFormat: false }
+    );
+    visitor.event(category, action, label, value, params).send();
+};
+
+module.exports = {
+    categories,
+    actions,
+    event,
+};
