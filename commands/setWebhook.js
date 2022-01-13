90ed35209110ff35984119b34b08c1f289622d20
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2022-01-13 19:54:22 +0300

    Set webhook using TelegramBot. Remove node-fetch package

--- commands/setWebhook.js
+++
@@ -1,4 +0,0 @@
-const fetch = require('node-fetch');
-
-const url = `https://api.telegram.org/bot${process.env.TOKEN}/setWebhook?url=${process.env.WEBAPP_URL}`;
-fetch(url).then((response) => console.log(response));
