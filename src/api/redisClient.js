a051e8354968ba8fe325cf2ad74a110c754d0f81
Ihar Makarski <22708426+makarsky@users.noreply.github.com>
2022-01-16 00:57:53 +0300

    Add sentry packages. Configure and enable error reporting and performance monitoring

--- src/api/redisClient.js
+++ src/api/redisClient.js
@@ -1,10 +1,14 @@
 const redis = require('redis');
+const Sentry = require('@sentry/node');
 
 const redisClient = redis.createClient({
   url: process.env.REDIS_URL,
   password: process.env.REDIS_PASSWORD,
 });
-redisClient.on('error', (err) => console.log('Redis redisClient Error', err));
+redisClient.on('error', (e) => {
+  console.log('Redis redisClient Error', e);
+  Sentry.captureException(e);
+});
 redisClient.connect();
 
 module.exports = {
